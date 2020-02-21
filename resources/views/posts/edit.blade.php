39274d1a2a8406d5902cd3ec05662cb117c228bf
kikiope Oyeniran <kikioyeniran@gmail.com>
2020-02-21 21:03:15 +0100

    First commit

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Create Posts</h1>
+    {!! Form::open(['action' => ['PostsController@update', $post->id], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+        <div class="form-group">
+            {{Form::label('title', 'Title')}}
+            {{Form::text('title', $post->title, ['class' => 'form-control', 'placeholder' => 'Title'])}}
+        </div>
+        <div class="form-group">
+            {{Form::label('body', 'Body')}}
+            {{Form::textarea('body', $post->body, ['class' => 'form-control', 'placeholder' => 'Body Text'])}}
+        </div>
+        <div class="form-group">
+            {{Form::file('cover_image')}}
+        </div>
+        {{Form::hidden('_method', 'PUT')}}
+        {{Form::submit('Submit', ['class'=> 'btn btn-primary'])}}
+    {!! Form::close() !!}
+    {{-- echo Form::open(['action' => 'PostController@store', 'method' => 'POST']) --}}
+@endsection
